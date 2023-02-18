3af70ff3b74e7e86aab63f3247d0bab3a8a12d53
InnocentAnywar <innocentkingsly69@gmail.com>
2023-02-18 06:03:13 -0800

    cisfun

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *
- *return: Always 0 (Success)
+ * main - Entry point
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
