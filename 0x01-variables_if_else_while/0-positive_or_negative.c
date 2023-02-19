f80e468a00e30b8cc5163ed55458c13d37d60971
InnocentAnywar <innocentkingsly69@gmail.com>
2023-02-19 14:23:16 -0800

    Cisfun

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * maim - Entry point
+ * main - Entry point
  * Return: Always 0 (success)
  */
 
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if( n > 0 )
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if( n == 0 )
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
