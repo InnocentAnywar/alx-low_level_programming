c4600dbc41d8a9c5944cfe3ca3d6efd0125a92a3
InnocentAnywar <innocentkingsly69@gmail.com>
2023-02-21 13:06:02 -0800

    cisfun

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char n = 'a';
+	int m = 0;
+
+	while (m < 10)
+	{
+		_putchar('\n');
+		m++;
+		while (n <= 'z')
+		{
+			_putchar(n);
+			n++;
+		}
+	}
+}
