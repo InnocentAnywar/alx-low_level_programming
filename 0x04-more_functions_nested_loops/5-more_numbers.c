7e93d44853b76aa3886e236a63c369e4a7e2a812
InnocentAnywar <innocentkingsly69@gmail.com>
2023-02-25 08:42:22 -0800

    Editing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,12 @@ void more_numbers(void)
 	for (m = 0; m <= 9; m++)
 	{
 		for (n = 0; n <= 14; n++)
-		{	
+		{
 			if (n > 9)
 			{
-				_putchar('0' + n / 10);
+				_putchar('0' + (n / 10));
 			}
-			_putchar('0' + n % 10);
+			_putchar('0' + (n % 10));
 		}
 		_putchar('\n');
 	}
