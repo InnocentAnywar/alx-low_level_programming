16b7114038bc83a9233f2e0086b7a1eae40f883d
InnocentAnywar <innocentkingsly69@gmail.com>
2023-03-20 13:33:52 -0700

    Amazing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printsname of program file
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+Innocent Anywar - +25678165062
+For more information contact me from the above address or use my Whatsapp +256707277450. Thank you.
