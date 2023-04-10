cd8049c29309fc7f4cc69d1965891b85a35061c6
InnocentAnywar <innocentkingsly69@gmail.com>
2023-04-10 22:18:10 +0300

    Done with first

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - converts binary to unsigned i
+ * @b: the binary string
+ * Return: the converted number or 0 for errors
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int l = strlen(b);
+	unsigned int decimal = 0;
+
+	for (i = 0; i < l; i++)
+	{
+		if (b[i] == '1')
+		{
+			decimal = decimal * 2 + 1;
+		}
+		else if (b[i] == '0')
+		{
+			decimal *= 2;
+		}
+		else if (b[i] != '1' || b[i] != '0')
+		{
+			return (0);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (decimal);
+}
